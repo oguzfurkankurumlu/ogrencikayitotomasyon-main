c101565ae305ed7d4f50350e2f7f4cef37434044
Oğuz Furkan Kurumlu <87278475+oguzfurkankurumlu@users.noreply.github.com>
2025-01-09 18:56:23 +0300

    smsmsms

--- obj/Debug/net9.0/summerschool.AssemblyInfo.cs
+++ obj/Debug/net9.0/summerschool.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("summerschool")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8a6ea68a25f3afb8c546cb5b53d00f00b2e0c93")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1d50cdc39e86907075b62237972b5454e17bc6b")]
 [assembly: System.Reflection.AssemblyProductAttribute("summerschool")]
 [assembly: System.Reflection.AssemblyTitleAttribute("summerschool")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
